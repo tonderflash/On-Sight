15ac3bc782f14960b12d7ab3154750652aa1014b
Israel Gabriel Garcia Reynoso <82509220+tonderflash@users.noreply.github.com>
2023-07-23 11:38:18 -0400

    added wait for db

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models # noqa
+"""
+Database models.
 
-# Create your models here.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create a new user profile."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
